518d9fc3a4f0f9cc54eb8806008c3bfff2c46cd6
BadApple <1076351865@qq.com>
2017-04-27 09:22:53 +0800

    从相册扫描二维码

---
+++ app/src/main/java/com/xbw/shadowsocks/core/TunnelFactory.java
@@ -0,0 +1,35 @@
+package com.xbw.shadowsocks.core;
+
+import com.xbw.shadowsocks.tunnel.Config;
+import com.xbw.shadowsocks.tunnel.RawTunnel;
+import com.xbw.shadowsocks.tunnel.Tunnel;
+import com.xbw.shadowsocks.tunnel.httpconnect.HttpConnectConfig;
+import com.xbw.shadowsocks.tunnel.httpconnect.HttpConnectTunnel;
+import com.xbw.shadowsocks.tunnel.shadowsocks.ShadowsocksConfig;
+import com.xbw.shadowsocks.tunnel.shadowsocks.ShadowsocksTunnel;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
+public class TunnelFactory {
+	
+	public static Tunnel wrap(SocketChannel channel, Selector selector){
+		return new RawTunnel(channel, selector);
+	}
+ 
+	public static Tunnel createTunnelByConfig(InetSocketAddress destAddress, Selector selector) throws Exception {
+		if(destAddress.isUnresolved()){
+			Config config=ProxyConfig.Instance.getDefaultTunnelConfig(destAddress);
+			if(config instanceof HttpConnectConfig){
+				return new HttpConnectTunnel((HttpConnectConfig)config,selector);
+			}else if(config instanceof ShadowsocksConfig){
+				return new ShadowsocksTunnel((ShadowsocksConfig)config,selector); 
+			} 
+			throw new Exception("The config is unknow.");
+		}else {
+			return new RawTunnel(destAddress, selector);
+		}
+	}
+
+}
